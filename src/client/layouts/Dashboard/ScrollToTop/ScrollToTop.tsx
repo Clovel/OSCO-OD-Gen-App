742a1ef6e762c473643010c84ccd7c96f54467b7
Clovis Durand <cd.clovel19@gmail.com>
2021-05-12 21:53:14 +0200

    Beautified ScrollToTop.tsx
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

--- src/client/layouts/Dashboard/ScrollToTop/ScrollToTop.tsx
+++ src/client/layouts/Dashboard/ScrollToTop/ScrollToTop.tsx
@@ -12,21 +12,20 @@ import { useLocation } from 'react-router-dom';
 /* Type definitions ------------------------------------ */
 
 /* withScrollToTopDeclaration HOC ---------------------- */
-// eslint-disable-next-line react/display-name
 export const withScrollToTopDeclaration = <T, R extends (HTMLElement | undefined | null)>(Component: React.FC<T>): React.FC<T> => {
   const lReturnComp: React.FC<T> = (props) => {
-  /** Own refs : refs of the Component. */
+    /** Own refs : refs of the Component. */
     const lOwnRefs = useRef<HTMLElement[]>([]);
 
     /** Get the current location in the web app.
-   * This value will be watched by a hook
-   * that will trigger a scroll to (0, 0)
-   */
+     * This value will be watched by a hook
+     * that will trigger a scroll to (0, 0)
+     */
     const lLoc = useLocation();
 
     /** This function allows us to add a component to the context.
-   * This will help us scroll to the top of each component.
-   */
+     * This will help us scroll to the top of each component.
+     */
     const addToScrollToTop = useCallback(
       (pRef: R) => {
       /* Check if pRef exists and if it has not yet been added to the refs */
@@ -39,19 +38,15 @@ export const withScrollToTopDeclaration = <T, R extends (HTMLElement | undefined
     );
 
     /** Declare a React hook to scroll to the top
-   * of the Component.
-   */
+     * of the Component.
+     */
     useEffect(
       () => {
-      /* Scroll to the top of the page */
+        /* Scroll to the top of the page */
         lOwnRefs.current.forEach(
           (pElmt: HTMLElement) => {
             if(pElmt !== undefined && pElmt !== null) {
-            /* eslint-disable @typescript-eslint/no-unsafe-call */
-            /* eslint-disable @typescript-eslint/no-unsafe-member-access */
               pElmt.scroll(0, 0);
-            /* eslint-enable @typescript-eslint/no-unsafe-call */
-            /* eslint-enable @typescript-eslint/no-unsafe-member-access */
             }
           },
         );
